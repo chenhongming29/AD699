3e28fba7036c79cb3d592c92e2889e7c573f27ba
Tia-Shen <53625400+Tia-Shen@users.noreply.github.com>
2019-08-06 12:09:32 -0400

    Update SemesterProject_map_visual.R

--- SemesterProject_map_visual.R
+++ SemesterProject_map_visual.R
@@ -2,6 +2,13 @@
 Seattle <- read_csv("seattle.csv")
 QueenAnne.tg = dplyr::filter(Seattle, neighbourhood_group_cleansed == "Queen Anne")
 
+#frequency Polygon
+qa.host.since.plot <- ggplot(QueenAnne.tg,aes(x=host_since))+geom_freqpoly(binwidth=10)
+qa.host.since.plot 
+#scatter plot
+ggplot(data=QueenAnne.tg, aes(x=review_scores_rating, y=reviews_per_month,color = room_type,shape = room_type)) + geom_point(size = 2) + xlim(c(70,100))+
+  ggtitle('The reviews per month vs. review scores')
+
 #install.packages("ggmap")
 library(ggmap)
 library(ggplot2)
@@ -14,6 +21,3 @@ qa.property.map<- qmplot(longitude, latitude,data = QueenAnne.tg,extent = "panel
                      xlab = "Longitude", ylab = "Latitude", size = 2) +ggtitle( 'Map of Property Type')
 qa.property.map
 
-#frequency Polygon
-qa.host.since.plot <- ggplot(QueenAnne.tg,aes(x=host_since))+geom_freqpoly(binwidth=10)
-qa.host.since.plot 
